f8c83c4a6a831377d5ac866f0cabb680752e2408
Myaser Waheed Ismail <myaser432007@gmail.com>
2025-04-11 21:15:07 +0400

    Update project.py

--- project.py
+++ project.py
@@ -1,26 +1,26 @@
 # checks the length of the password and if it has any lower and upper letters,
-def check_password(password):
-    strength = 0
+def check_the_password(password):
+    strength_points = 0
     if len(password) >= 8:
-        strength = strength + 1
+        strength_points = strength_points + 1
     if any(char.islower() for char in password):
-        strength = strength + 1
+        strength_points = strength_points + 1
     if any(char.isupper() for char in password):
-        strength = strength + 1
+        strength_points = strength_points + 1
 
     # checks if the password contains numbers
 
     if any(char.isdigit() for char in password):
-        strength = strength + 1
+        strength_points = strength_points + 1
 
     # calculates the strength points of the password
-    if strength == 4:
+    if strength_points == 4:
         print("💪 you have a Strong password")
-    elif strength == 3:
+    elif strength_points == 3:
         print("🟡 your password is Medium")
     else:
         print("🔴 your password is Weak")
 
 # asks the user to enter his password
 pas = input("Enter Your Password")
-check_password(pas)
+check_the_password(pas)
